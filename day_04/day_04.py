d355fa543382802265ccfab53d19965662f92f42
Roibos22 <grimmeisen.leon@gmail.com>
2024-12-04 12:26:00 +0100

    refactor day_04.py

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -1,100 +1,62 @@
 data = []
-str = "XMAS"
 
 with open('input.txt', 'r') as file:
     data = [line.strip() for line in file]
-
-# for i in range(len(data)):
-#     print(data[i])
-
-count = 0
 
 def in_range(y, x):
     return (y >= 0 and y < len(data) and 
             x >= 0 and x < len(data[y]))
 
-def check_xmas(y, x):
-    global count
+# PART 1
 
-    if in_range(y - 1, x - 1) and data[y - 1][x - 1] == 'M':
-       if in_range(y - 2, x - 2) and data[y - 2][x - 2] == 'A':
-           if in_range(y - 3, x - 3) and data[y - 3][x - 3] == 'S':
-               count += 1
+directions_part_1 = [
+    (-1, -1), (+0, -1), (+1, -1),
+    (-1, +0),           (+1, +0),
+    (-1, +1), (+0, +1), (+1, +1)
+]
 
-    if in_range(y - 1, x) and data[y - 1][x] == 'M':
-        if in_range(y - 2, x) and data[y - 2][x] == 'A':
-            if in_range(y - 3, x) and data[y - 3][x] == 'S':
+def check_xmas(y, x, count):
+    str = "XMAS"
+
+    for dx, dy in directions_part_1:
+        tmp_x, tmp_y = x, y
+        for i in range(len(str)):
+            if not in_range(tmp_y, tmp_x) or str[i] != data[tmp_y][tmp_x]:
+                break
+            tmp_y += dy; tmp_x += dx
+            if i == len(str) - 1:
                 count += 1
-               
-    if in_range(y - 1, x + 1) and data[y - 1][x + 1] == 'M':
-        if in_range(y - 2, x + 2) and data[y - 2][x + 2] == 'A':
-            if in_range(y - 3, x + 3) and data[y - 3][x + 3] == 'S':
-                count += 1
-               
-    if in_range(y, x - 1) and data[y][x - 1] == 'M':
-        if in_range(y, x - 2) and data[y][x - 2] == 'A':
-            if in_range(y, x - 3) and data[y][x - 3] == 'S':
-               count += 1
-               
-    if in_range(y, x + 1) and data[y][x + 1] == 'M':
-        if in_range(y, x + 2) and data[y][x + 2] == 'A':
-            if in_range(y, x + 3) and data[y][x + 3] == 'S':
-                count += 1
-               
-    if in_range(y + 1, x - 1) and data[y + 1][x - 1] == 'M':
-        if in_range(y + 2, x - 2) and data[y + 2][x - 2] == 'A':
-            if in_range(y + 3, x - 3) and data[y + 3][x - 3] == 'S':
-                count += 1
-               
-    if in_range(y + 1, x) and data[y + 1][x] == 'M':
-       if in_range(y + 2, x) and data[y + 2][x] == 'A':
-           if in_range(y + 3, x) and data[y + 3][x] == 'S':
-               count += 1
-               
-    if in_range(y + 1, x + 1) and data[y + 1][x + 1] == 'M':
-        if in_range(y + 2, x + 2) and data[y + 2][x + 2] == 'A':
-            if in_range(y + 3, x + 3) and data[y + 3][x + 3] == 'S':
-                count += 1
+    return count
 
+# PART 2
 
-for y in range(len(data)):
-    for x in range(len(data[y])):
-        if data[y][x] == 'X':
-            # print(y , x)
-            # check_char(y, x, 1)
-            check_xmas(y, x)
+directions_part_2 = [
+    (-1, -1), (+1, -1),
+    (-1, +1), (+1, +1)
+]
 
-print("#1 ->", count)
-count = 0
+def check_x_mas(y, x, count):
+    if not data[y][x] == 'A': return count
 
-def check_x_mas(y, x):
-    global count
+    matches = 0
+    for dx, dy in directions_part_2:
+        if (in_range(y + dy, x + dx) and data[y + dy][x + dx] == 'M') and \
+           (in_range(y - dy, x - dx) and data[y - dy][x - dx] == 'S'):
+            matches += 1
 
-    # top two
-    if in_range(y - 1, x - 1) and data[y - 1][x - 1] == 'M' and in_range(y - 1, x + 1) and data[y - 1][x + 1] == 'M':
-        if in_range(y + 1, x - 1) and data[y + 1][x - 1] == 'S' and in_range(y + 1, x + 1) and data[y + 1][x + 1] == 'S':
-            count += 1
+    if matches == 2: 
+        return count + 1
 
-    # left two
-    if in_range(y - 1, x - 1) and data[y - 1][x - 1] == 'M' and in_range(y + 1, x - 1) and data[y + 1][x - 1] == 'M':
-        if in_range(y - 1, x + 1) and data[y - 1][x + 1] == 'S' and in_range(y + 1, x + 1) and data[y + 1][x + 1] == 'S':
-            count += 1
+    return count
 
-    #right two
-    if in_range(y - 1, x + 1) and data[y - 1][x + 1] == 'M' and in_range(y + 1, x + 1) and data[y + 1][x + 1] == 'M':
-        if in_range(y - 1, x - 1) and data[y - 1][x - 1] == 'S' and in_range(y + 1, x - 1) and data[y + 1][x - 1] == 'S':
-            count += 1
+def main():
+    count_part_1 = 0
+    count_part_2 = 0
+    for y in range(len(data)):
+        for x in range(len(data[y])):
+            count_part_1 = check_xmas(y, x, count_part_1)
+            count_part_2 = check_x_mas(y, x, count_part_2)
+    print("#1 ->", count_part_1)
+    print("#2 ->", count_part_2)
 
-    # bottom two
-    if in_range(y + 1, x - 1) and data[y + 1][x - 1] == 'M' and in_range(y + 1, x + 1) and data[y + 1][x + 1] == 'M':
-        if in_range(y - 1, x - 1) and data[y - 1][x - 1] == 'S' and in_range(y - 1, x + 1) and data[y - 1][x + 1] == 'S':
-            count += 1
-
-
-for y in range(len(data)):
-    for x in range(len(data[y])):
-        if data[y][x] == 'A':
-            check_x_mas(y, x)
-
-
-print("#2 ->", count)
+main()
