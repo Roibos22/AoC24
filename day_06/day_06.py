d213803707827e9c67a70887c3a0878003757da5
LofiLeon <grimmeisen.leon@gmail.com>
2024-12-06 21:05:11 +0100

    day_06.py

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -1,31 +1,25 @@
-map = [list(line.strip()) for line in open('input.txt')]
-
-pos = [0, 0]
-dir = None
+original_map = [list(line.strip()) for line in open('input.txt')]
 directions = [
-        ['^', ( 0, -1)],
-        ['>', ( 1,  0)],
-        ['v', ( 0, +1)],
-        ['<', (-1,  0)],
+        ['^', ( 0, -1), '|'],
+        ['>', ( 1,  0), '-'],
+        ['v', ( 0, +1), '|'],
+        ['<', (-1,  0), '-'],
     ]
+pos = [0, 0]
+
+def print_map(map):
+    for i, line in enumerate(map):
+        print(f"{i:2} ", end='')  # Row number with 2 spaces for alignment
+        print(''.join(line), end='\n\n')
+    
 
 def find_pos(map):
-    global pos, dir
     for y, line in enumerate(map):
         for x, char in enumerate(line):
             for d in directions:
                 if d[0] == char:
-                    dir = d
-                    pos[0] = x
-                    pos[1] = y
-                    break
-            # if char in ' <>^v':
-            #     pos[0] = x
-            #     pos[1] = y
-            #     dir = map[y][x]
-            #     return
-
-find_pos(map)
+                    return d, [x, y]
+    return None, None
 
 def in_range(map, x, y):
     return (
@@ -35,33 +29,77 @@ def in_range(map, x, y):
         y < len(map)
     )
 
-while(in_range(map, pos[0], pos[1])):
+walked_positions_part_1 = set()
 
-    # if map[pos[1] + dir[1][1]][pos[0] + dir[1][0]]
-    print("y ", pos[1] + dir[1][1])
-    print("x", pos[0] + dir[1][0])
-    if in_range(map, pos[1] + dir[1][1], pos[0] + dir[1][0]):
-        if map[pos[1] + dir[1][1]][pos[0] + dir[1][0]] == '#':
-            cur_i = directions.index(dir)
-            next_i = (cur_i + 1) % len(directions)
-            dir = directions[next_i]
+def part_one():
+    global directions, walked_positions_part_1
+    map = [row[:] for row in original_map]
+    dir, pos = find_pos(map)
+    while(in_range(map, pos[0], pos[1])):
+        next_y = pos[1] + dir[1][1]
+        next_x = pos[0] + dir[1][0]
+            
+        if in_range(map, next_x, next_y):
+            if map[next_y][next_x] == '#':
+                cur_i = directions.index(dir)
+                next_i = (cur_i + 1) % len(directions)
+                dir = directions[next_i]
 
-    map[pos[1]][pos[0]] = 'x'
-    pos[1] = pos[1] + dir[1][1]
-    pos[0] = pos[0] + dir[1][0]
+        if not map[pos[1]][pos[0]] == 'x':
+            map[pos[1]][pos[0]] = 'x'
+            walked_positions_part_1.add((pos[0], pos[1]))
+        pos[1] += dir[1][1]
+        pos[0] += dir[1][0]
 
-res1 = sum(row.count('x') for row in map)
+    return len(walked_positions_part_1)
 
-print("#1 -> ", res1)
+def is_visited(visited, pos, dir_char):
+    pos_with_dir = (pos[0], pos[1], dir_char)
+    return pos_with_dir in visited
 
-    # match dir[0]:
-    #     case '^':
-    #         map[pos[1]][pos[0]] = 'x'
-    #         pos[1] = pos[1] - 1
+def is_infinite_loop(map, init_pos, dir):
+    pos = (init_pos[0], init_pos[1], dir[0])
+    visited = set()
 
+    while(in_range(map, pos[0], pos[1])):
 
-print(dir)
-print(pos)
+        if pos in visited:
+            return 1
+        visited.add(pos)
 
-for line in map:
-    print(line)
+        # mark position
+        if map[pos[1]][pos[0]] in '-|+':
+            map[pos[1]][pos[0]] = '+'
+        elif map[pos[1]][pos[0]] == '.':
+            map[pos[1]][pos[0]] = dir[2]
+
+        # change direction if obstacle
+        if in_range(map, pos[0] + dir[1][0], pos[1] + dir[1][1]):
+            while map[pos[1] + dir[1][1]][pos[0] + dir[1][0]] == '#':
+                map[pos[1]][pos[0]] = '+'
+                dir = directions[(directions.index(dir) + 1) % len(directions)]
+
+        pos = (pos[0] + dir[1][0], pos[1] + dir[1][1], dir[0])
+    return 0
+
+def part_two():
+    count = 0
+    map = [row[:] for row in original_map]
+    initial_dir, initial_pos = find_pos(map)
+    
+    for y in range(len(map)):
+        for x in range(len(map[y])):
+          if (x, y) in walked_positions_part_1:
+                tmp_pos = initial_pos[:]
+                tmp_dir = initial_dir[:]
+                tmp_map = [row[:] for row in map]
+                tmp_map[y][x] = '#'
+                tmp_map[tmp_pos[1]][tmp_pos[0]] = '.'
+                count += is_infinite_loop(tmp_map, tmp_pos, tmp_dir)
+    return count
+
+def main():
+    print("#1 -> ", part_one())
+    print("#2 -> ", part_two())
+
+main()
