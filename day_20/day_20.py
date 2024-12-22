e1cd894f4174f3f3c15f3da5035273756a0b9d04
LofiLeon <grimmeisen.leon@gmail.com>
2024-12-22 21:54:45 +0100

    day_20.py still failing

--- day_20/day_20.py
+++ day_20/day_20.py
@@ -36,7 +36,6 @@ def bfs(map, start):
             for i, p in enumerate(path):
                 map[p[1]][p[0]] = str(i)
             return path
-
         for dx, dy in dirs:
             next = (pos[0] + dx, pos[1] + dy)
             if in_range(next[1], next[0], map) and map[next[1]][next[0]] != '#' and next not in visited:
@@ -45,24 +44,28 @@ def bfs(map, start):
 
     print(dq)
 
+
+seen = set()
 def find_targets_in_range(pos, delay, map, targets=None):
+   #print(pos, delay)
     if targets is None: targets = set()
+    targets.add((pos, 20 - delay))
     if delay <= 0: return targets
 
     for dx, dy in dirs:
         next = (pos[0] + dx, pos[1] + dy)
-        if in_range(next[0], next[1], map) and next not in targets:
-            targets.add(next)
+        if in_range(next[0], next[1], map) and (next, delay - 1) not in targets:
             find_targets_in_range(next, delay - 1, map, targets)
     return targets
 
 def find_shortcuts(path, map, delay):
     shortcuts = {}
     for pos in path:
+        #print(pos)
         targets = find_targets_in_range(pos, delay, map)
         for target in targets:
-            if  map[target[1]][target[0]] != '#':
-                saved = int(map[target[1]][target[0]]) - int(map[pos[1]][pos[0]]) - 2
+            if  map[target[0][1]][target[0][0]] != '#':
+                saved = int(map[target[0][1]][target[0][0]]) - int(map[pos[1]][pos[0]]) - 2
                 if saved > 0:
                     shortcuts[saved] = shortcuts.get(saved, 0) + 1
     return shortcuts
@@ -72,9 +75,12 @@ def find_shortcuts_2(path, map, delay):
     for pos in path:
         #print(pos)
         targets = find_targets_in_range(pos, delay, map)
+        #print(targets)
+        print("---")
         for target in targets:
-            if  map[target[1]][target[0]] != '#':
-                saved = int(map[target[1]][target[0]]) - int(map[pos[1]][pos[0]]) - 2
+            if  map[target[0][1]][target[0][0]] != '#':
+
+                saved = int(map[target[0][1]][target[0][0]]) - int(map[pos[1]][pos[0]]) - (20 - target[1])
                 if saved > 0:
                     shortcuts[saved] = shortcuts.get(saved, 0) + 1
     return shortcuts
@@ -85,12 +91,44 @@ def part_one():
     shortcuts = find_shortcuts(path, map, 2)
     return sum(value for key, value in shortcuts.items() if key >= 100)
 
+def add_path_to_map(map, path):
+    for i, pos in enumerate(path):
+        map[pos[1]][pos[0]] = i
+    return map
+
+def get_targets(path, map, delay):
+    targets = [] # (pos, [targets])
+    for pos in path:
+        pos_targets = set()
+        for s in range(1, delay):
+            for dx in range(s):
+                dy = s - dx
+                for x, y in {(pos[0] + dx, pos[1] + dy), (pos[0] - dx, pos[1] + dy), (pos[0] + dx, pos[1] - dy), (pos[0] - dx, pos[1] - dy)}:
+                    if in_range(x, y, map) and map[y][x] != '#':
+                        pos_targets.add((x, y))
+        print((pos, pos_targets))
+        targets.append((pos, pos_targets))
+    return targets
+
 def part_two():
+    shortcuts = {}
     map = [list(line.strip()) for line in open(filename)]
     path = bfs(map, get_start(map))
-    shortcuts = find_shortcuts_2(path, map, 20)
+    targets = get_targets(path, map, 20)
+    map = add_path_to_map(map, path)
+    for pos_and_targets in targets:
+        #print(pos_and_targets)
+        pos = pos_and_targets[0]
+        for target in pos_and_targets[1]:
+            #print(target)
+            saved = map[target[1]][target[0]] - map[pos[1]][pos[0]] - 20
+            if saved > 0:
+                shortcuts[saved] = shortcuts.get(saved, 0) + 1
     print(shortcuts)
-    return sum(value for key, value in shortcuts.items() if key >= 50)
+    #print_map(map)
+    #shortcuts = find_shortcuts_2(path, map, 20)
+    #print("Shortcuts:", shortcuts)
+    #return sum(value for key, value in shortcuts.items() if key >= 50)
 
 def main():
     print("#1 -> ", part_one())
